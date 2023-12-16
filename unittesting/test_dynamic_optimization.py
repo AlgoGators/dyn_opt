2a2342596b23f0e7c66cff8910f6ffd405634fad
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-12-16 11:06:37 -0500

    Processed the data for returns_df

--- unittesting/test_dynamic_optimization.py
+++ unittesting/test_dynamic_optimization.py
@@ -6,10 +6,15 @@ sys.path.append('../DYN_OPT')
 import unittest
 
 from dynamic_optimization import *
+from general_functions import get_daily_returns
 
 class TestDynamicOptimization(unittest.TestCase):
     def setUp(self):
-        pass
+        MES_returns = get_daily_returns(pd.read_csv('unittesting/data/_MES_Data.csv'), return_column="MES")
+        ZF_returns = get_daily_returns(pd.read_csv('unittesting/data/_ZF_Data.csv'), return_column="ZF")
+        ZN_returns = get_daily_returns(pd.read_csv('unittesting/data/_ZN_Data.csv'), return_column="ZN")
+
+        self.returns_df = MES_returns.merge(ZF_returns, on='Date', how="inner").merge(ZN_returns, on='Date', how="inner")
 
     def test_get_optimal_position(self):
         expected_result = {'ES' : 1, 'ZN' : 0}
@@ -23,7 +28,8 @@ class TestDynamicOptimization(unittest.TestCase):
             optimal_positions=optimal_positions, 
             notional_exposures_per_contract=notional_exposures_per_contract,
             capital=capital, 
-            costs_per_contract=costs_per_contract)
+            costs_per_contract=costs_per_contract,
+            returns_df=self.returns_df)
         
         self.assertEqual(result, expected_result)
 
