9df0a4066e4dfad1c1507c0119dc0b733bcbeab9
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-01-02 09:44:13 -0500

    Update statistical_functions.py
    Removed some exponentially weighted functions that werent being used
    
    Changed correlation matrix to a simple pandas correlation matrix since it was built with an exponentially weighted system in mind but this is no longer applicable.

--- statistical_functions.py
+++ statistical_functions.py
@@ -157,76 +157,6 @@ def exponentially_weighted_stddev(
     return ew_stddev * factor
 
 
-def exponentially_weighted_var(
-    lst : list[float],
-    alpha : float = None,
-    span : int = None,
-    threshold : int = 10) -> float:
-
-    """
-        threshold : when the formula switches from a simple variance to EW_var
-    """
-
-    # checks that only one of the variables is given
-    if not(any([span, alpha]) and not all([span, alpha])):
-        raise ValueError("Only one of span or alpha may be used")
-    
-    if alpha is None:
-        alpha : float = 2 / (span + 1)
-
-    lst_len : int = len(lst)
-
-
-    # if the list length is less than threshold just return the simple variance (stddev squared)
-    if lst_len <= threshold:
-        return VAR(lst=lst)
-    
-
-    # EWV(t) = λ * (x(t) − EWMA(t−1))^2 + (1 − λ) * EWV(t−1)
-    # starting value is just the simple variance of the first 10 values
-    # EW_var = stddev(lst[:10])**2
-
-    # starting with n = 10 (threshold)
-    # EW_var = alpha * (lst[n] - EWMA(lst[:n], alpha=alpha))**2 + (1-alpha) * EW_var
-    
-    EW_var : float = VAR(lst[:threshold])
-
-    for n in range(threshold, lst_len):
-        EW_var = alpha * (lst[n] - EWMA(lst[:n], alpha=alpha))**2 + (1 - alpha) * EW_var
-
-    return EW_var
-
-
-def exponentially_weighted_covar(
-    lst_X : list[float],
-    lst_Y : list[float],
-    alpha : float = None,
-    span : int = None) -> float:
-    """Calculates an exponentially weighted covariance with weight alpha"""
-
-    #@ EW Cov(X, Y) = sum(0, t) (1 - α)^n * (X(t-1) - EWMA(X)) * (Y(t-1) - EWMA(Y))
-
-    # checks that only one of the variables is given
-    if not(any([span, alpha]) and not all([span, alpha])):
-        raise ValueError("Only one of span or alpha may be used")
-    
-    if alpha is None:
-        alpha : float = 2 / (span + 1)
-
-    EW_covar : float = 0
-
-    lst_len : int = len(lst_X)
-    last_IDX : int = lst_len - 1
-
-    ewma_X : float = EWMA(lst_X, alpha=alpha)
-    ewma_Y : float = EWMA(lst_Y, alpha=alpha)
-
-    for n in range(0, lst_len):
-        EW_covar += (1-alpha)**n * (lst_X[last_IDX-n] - ewma_X) * (lst_Y[last_IDX-n] - ewma_Y)
-
-    return EW_covar
-
-
 def correlation(
     returns_X : pd.DataFrame,
     returns_Y : pd.DataFrame) -> float:
@@ -247,35 +177,17 @@ def correlation(
     return rho
 
 
-def exponentially_weighted_correlation(
-    returns_X : pd.DataFrame,
-    returns_Y : pd.DataFrame,
-    span : int) -> float:
-    
-    covar : float = exponentially_weighted_covar(returns_X, returns_Y, span=span)
-
-    var_tickerX : float = exponentially_weighted_var(returns_X, span=span)
-    var_tickerY : float = exponentially_weighted_var(returns_Y, span=span)
-
-
-    # default value for correlation should be nan just in case variance is 0
-    correlation : float = np.nan 
-    
-    # make sure neither variance is 0, if so then calculate the correlation
-    if (0 not in [var_tickerX, var_tickerY]):
-        correlation = covar / (sqrt(var_tickerX * var_tickerY))
-
-    return correlation
-
-
 def correlation_matrix(
     returns_df : pd.DataFrame,
     period : Periods,
-    window : int) -> np.array:
+    window : int,
+    tickers : list = None) -> np.array:
 
     periodic_returns_df = pd.DataFrame()
     
-    tickers = returns_df.columns.tolist()
+    if tickers is None:
+        tickers = returns_df.columns.tolist()
+        tickers.sort()
 
     for ticker in tickers:
         returns = returns_df[ticker].tolist()
@@ -284,125 +196,7 @@ def correlation_matrix(
         periodic_returns_df[ticker] = [sum(returns[x : x + period.value])
                                 for x in range(0, len(returns), period.value)][:-window]
 
-    correlation_matrix : list[float] = []
-
-    for n, tickerX in enumerate(tickers):
-        # always start with 1.0 in the correlations since X1 is perfectly correlated with X2
-        correlation_lst : list[float] = [1.0]
-
-        # go through the remaining variables, ignoring those previously calculated
-        for tickerY in tickers[n+1:]:
-            rho = correlation(periodic_returns_df[tickerX], periodic_returns_df[tickerY])
-
-            correlation_lst.append(rho)
-
-        correlation_matrix.append(correlation_lst)
-
-    #? right now we these correlations
-    #? coordinates are relative to the existing values
-    #?     0   1   2   3   4
-    #?     A   B   C   D   E
-    #? 0 A 1.0 0,1 0,2 0,3   
-    #? 1 B 0.5 1.0 1,1 1,2
-    #? 2 C 0.6 0.7 1.0 2,1
-    #? 3 D 0.4 0.9 0.4 1.0
-    #? 4 E 0.8 0.7 0.9 0.4 1.0
-
-    correlation_df = pd.DataFrame(columns=tickers)
-
-    for n, column in enumerate(correlation_matrix):
-        # we need the second value in each column to the left 
-
-        current_ticker : str = tickers[n]
-
-        if (n == 0):
-            correlation_df[current_ticker] = column
-            continue
-
-        new_column = []
-
-        row : int = n
-
-        for x in range(0,n):
-            new_column.append(correlation_matrix[x][row])
-            row -= 1
-
-        new_column.extend(column)
-
-        correlation_df[current_ticker] = new_column
-
-    return np.array(correlation_df)
-
-
-def exponentially_weighted_correlation_matrix(
-    returns_df : pd.DataFrame,
-    period : Periods,
-    span : int) -> np.array:
-
-    #@ correlation formula
-    #@ r =             EW Cov(X,Y)
-    #@     -------------------------------
-    #@      ______________________________
-    #@     √    EW Var(X) * EW Var(Y)
-
-    periodic_returns_df = pd.DataFrame()
-    
-    tickers = returns_df.columns.tolist()
-
-    for ticker in tickers:
-        returns = returns_df[ticker].tolist()
-
-        periodic_returns_df[ticker] = [sum(returns[x : x + period.value])
-                                for x in range(0, len(returns), period.value)]
-
-    correlation_matrix : list[float] = []
-
-    for n, tickerX in enumerate(tickers):
-        # always start with 1.0 in the correlations since X1 is perfectly correlated with X2
-        correlations : list[float] = [1.0]
-
-        # go through the remaining variables, ignoring those previously calculated
-        for tickerY in tickers[n+1:]:
-            correlation = exponentially_weighted_correlation(periodic_returns_df[tickerX], periodic_returns_df[tickerY], span=span)
-
-            correlations.append(correlation)
-
-        correlation_matrix.append(correlations)
-
-    #? right now we these correlations
-    #? coordinates are relative to the existing values
-    #?     0   1   2   3   4
-    #?     A   B   C   D   E
-    #? 0 A 1.0 0,1 0,2 0,3   
-    #? 1 B 0.5 1.0 1,1 1,2
-    #? 2 C 0.6 0.7 1.0 2,1
-    #? 3 D 0.4 0.9 0.4 1.0
-    #? 4 E 0.8 0.7 0.9 0.4 1.0
-
-    correlation_df = pd.DataFrame(columns=tickers)
-
-    for n, column in enumerate(correlation_matrix):
-        # we need the second value in each column to the left 
-
-        current_ticker : str = tickers[n]
-
-        if (n == 0):
-            correlation_df[current_ticker] = column
-            continue
-
-        new_column = []
-
-        row : int = n
-
-        for x in range(0,n):
-            new_column.append(correlation_matrix[x][row])
-            row -= 1
-
-        new_column.extend(column)
-
-        correlation_df[current_ticker] = new_column
-
-    return np.array(correlation_df)
+    return periodic_returns_df.corr()
 
 
 def rolling_std(
@@ -440,7 +234,8 @@ def rolling_std(
 
 
 def portfolio_covar( 
-        position_percent_returns : pd.DataFrame) -> np.array:
+    position_percent_returns : pd.DataFrame,
+    tickers = None) -> np.array:
     """Calculates a covariance matrix as outlined by Carver on pages 606-607"""
 
     #@ Σ = σ.ρ.σᵀ = σσᵀ ⊙ ρ (using Hadamard product) = Diag(σ) * ρ * Diag(σ)
@@ -453,20 +248,23 @@ def portfolio_covar(
 
     stddev_lst = []
 
-    tickers = position_percent_returns.columns.tolist()
+    if tickers is None:
+        tickers = position_percent_returns.columns.tolist()
+        tickers.sort()
 
     for ticker in tickers:
         # get the most recent value
         rolling_stddev = rolling_std(position_percent_returns[ticker])[-1]
         stddev_lst.append(rolling_stddev)
 
+    # this is in the same order as the corr_matrix
     stddev_array = np.array(stddev_lst)
 
-    corr_matrix = correlation_matrix(position_percent_returns, Periods.WEEKLY, 52)
+    corr_matrix = correlation_matrix(position_percent_returns, Periods.WEEKLY, 52, tickers)
 
-    EW_covar = np.dot(np.dot(np.diag(stddev_array), corr_matrix), np.diag(stddev_array))
+    covar = np.dot(np.dot(np.diag(stddev_array), corr_matrix), np.diag(stddev_array))
 
-    return EW_covar
+    return covar
 
 
 def portfolio_stddev(
